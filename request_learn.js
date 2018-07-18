3303cea3014ff21f311d0d6b6de6107f0947fd79
James <j1459.j1459@gmail.com>
2018-07-19 05:08:34 +0800

    Iteration works at this point

--- request_learn.js
+++ request_learn.js
@@ -4,27 +4,23 @@ const request = require('request')
 const jsonFile = require('jsonfile')
 const fs = require('fs-extra');
 const lupus = require('lupus');
-var RateLimiter = require('limiter').RateLimiter;
-var limiter = new RateLimiter(1, 10);
+//var RateLimiter = require('limiter').RateLimiter;
+//var limiter = new RateLimiter(1, 10);
 
 var threadData = jsonFile.readFileSync('git_ignored\\test_thread.json');
 
 const boardName = 'g'
 
+posts = threadData.posts
+downladThreadImages(posts)
 
-downladThreadImages(threadData)
 
-
-function downladThreadImages(threadData) {
-  console.log('downladThreadImages() threadData', threadData)
-  for (i in threadData.posts) {
-    console.log('i', i)
-    console.log('threadData.posts[i]', threadData.posts[i])
-    var postData = threadData.posts[i]
-    console.log('postData', postData)
-    limiter.removeTokens(1, () => {
-      downloadPostImage(postData)
-    })
+function downladThreadImages(posts) {
+  // console.log('downladThreadImages() posts', posts)
+  for (let i = 0; i< posts.length; i++){
+    const post = posts[i];
+    // console.log('post', post)
+    downloadPostImage(post)
   }
 }
 
@@ -32,13 +28,13 @@ function downladThreadImages(threadData) {
 
 
 function downloadPostImage(postData) {
-  console.log('downloadPostImage() postData', postData)
+  // console.log('downloadPostImage() postData', postData)
   // console.log('postData.filename', postData.filename)
   if (! postData.filename) {
     return
   } else {
     var fullURL = `https://i.4cdn.org/${boardName}/${postData.tim}${postData.ext}`
-    var fullFilePath = `debug/${boardName}/${postData.tim}.${postData.ext}`
+    var fullFilePath = `debug/${boardName}/${postData.tim}${postData.ext}`
     downloadMedia(fullURL, fullFilePath)
   }
 }
